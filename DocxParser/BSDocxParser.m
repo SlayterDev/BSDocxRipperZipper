f1f227eb64cb061f5a71222f13a514f44d5c054d
Bradley Slayter <Slayter@server.local>
2013-11-06 09:12:01 -0600

    Changed and moved some comments Run Strings now use NSMutableAttributedString Applying bold attributes no longer reallocates the entire string Added a category for NSMutableAttributedString to get full NSRange of string

--- DocxParser/BSDocxParser.m
+++ DocxParser/BSDocxParser.m
@@ -8,6 +8,16 @@
 
 #import "BSDocxParser.h"
 
+@interface NSMutableAttributedString (RangeExtension)
+- (NSRange)fullRange;
+@end
+
+@implementation NSMutableAttributedString (RangeExtension)
+- (NSRange)fullRange {
+	return (NSRange){0, self.string.length};
+}
+@end
+
 @implementation BSDocxParser
 
 -(id) initWithFileURL:(NSURL *)fileURL {
@@ -36,32 +46,35 @@
         return;
     }
     
-    //NSLog(@"%@", self.xmlDoc.rootElement);
     finalString = [[NSMutableAttributedString alloc] initWithString:@""];
-    //[[finalString mutableString] setString:@""];
     [self loadString];
 }
 
 -(void) loadString {
-    GDataXMLElement *body = [[self.xmlDoc.rootElement elementsForName:@"w:body"] objectAtIndex:0]; // get body element
-    NSArray *paragraphs = [body elementsForName:@"w:p"]; // find all parapgraphs
+	// get body element
+    GDataXMLElement *body = [[self.xmlDoc.rootElement elementsForName:@"w:body"] objectAtIndex:0];
+    
+	NSArray *paragraphs = [body elementsForName:@"w:p"]; // find all parapgraphs
     for (GDataXMLElement *paragraph in paragraphs) {
         NSLog(@"Found a paragraph!");
         
         NSArray *runs = [paragraph elementsForName:@"w:r"]; // find all runs in each paragraph
         for (GDataXMLElement *run in runs) {
             NSLog(@"Found a run!");
-            GDataXMLElement *runTextElem = [[run elementsForName:@"w:t"] objectAtIndex:0];   // get the text of it exists
+			// get the text if it exists
+            GDataXMLElement *runTextElem = [[run elementsForName:@"w:t"] objectAtIndex:0];
             if (runTextElem) {
-                //finalString = [finalString stringByAppendingString:runTextElem.stringValue]; // add text from run to final string
-                NSAttributedString *runString = [[NSAttributedString alloc] initWithString:runTextElem.stringValue attributes:@{NSFontAttributeName: normalFont}];
+				// Create attr string from text in run
+                NSMutableAttributedString *runString = [[NSMutableAttributedString alloc] initWithString:runTextElem.stringValue attributes:@{NSFontAttributeName: normalFont}];
                 
+				// Pull out bold attribute tag
                 GDataXMLElement *rPr = [[run elementsForName:@"w:rPr"] objectAtIndex:0];
                 GDataXMLElement *boldTrait = [[rPr elementsForName:@"w:b"] objectAtIndex:0];
                 if (boldTrait.XMLString) {
                     if ([boldTrait.XMLString hasSuffix:@"w:val=\"1\"/>"]) {
                         NSLog(@"SET BOLD YES");
-                        runString = [[NSAttributedString alloc] initWithString:runTextElem.stringValue attributes:@{NSFontAttributeName: boldFont}];
+						// Give the string bold attributes
+                        [runString setAttributes:@{NSFontAttributeName: boldFont} range:[runString fullRange]];
                         NSLog(@"Run: %@", runString.string);
                     }
                 }
